153d48e52fda4415a4ba7ed98f41d40165101e8f
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-03-15 12:03:36 -0300

    Add comments

--- bin/app.py
+++ bin/app.py
@@ -4,10 +4,10 @@ import requests
 from bs4 import BeautifulSoup
 
 def main():
-    page = requests.get("https://www.sammobile.com/samsung/galaxy-s9/firmware/SM-G9600/ZTO/")
+    page = requests.get("https://www.sammobile.com/samsung/galaxy-s9/firmware/SM-G9600/ZTO/") # Replace with your device's URL on Sammobile
     soup = BeautifulSoup(page.content, "html.parser")
     soup = soup.findAll("span")
-    print("Latest found firmware for SM-G9600:")
+    print("Latest found firmware for SM-G9600:") # Replace "SM-G9600" with your device model
     print(" Date: " + str(soup[6]).replace("<span>", "").replace("</span>", ""))
     print(" Android Version: " + str(soup[7]).replace("<span>", "").replace("</span>", ""))
     print(" PDA: " + str(soup[8]).replace("<span>", "").replace("</span>", ""))
